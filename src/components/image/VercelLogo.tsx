7a2938a31f25524034ccd9444fc5f156224c1775
hansol <wkdqhrh00@naver.com>
2023-01-25 23:49:20 +0900

    Vercel Main Page Header 따라하기

---
+++ src/components/image/VercelLogo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function VercelLogo() {
+    return (
+        <Image src="/vercel-logotype-light.svg" alt="vercel logo" width="114" height="26"/>
+    )
+}
--- src/pages/vercel/index.tsx
+++ src/pages/vercel/index.tsx
@@ -1,9 +1,67 @@
 import styles from '@/pages/vercel/Vercel.module.scss'
+import VercelLogo from "@/components/image/VercelLogo";
 
 export default function Vercel() {
-    return (
-        <>
-            <div className={styles.text}>Vercel</div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        {/* Header */}
+        <div className={styles['navbar-wrapper']}>
+          <header className={styles['navbar']}>
+            {/* Logo */}
+            <div className={styles['navbar-logo']}>
+              <VercelLogo/>
+            </div>
+
+            {/* menu */}
+            <div className={styles['navbar-main']}>
+              <ul>
+                <li>
+                  <a href='#'>Features</a>
+                </li>
+                <li>
+                  <a href='#'>Docs</a>
+                </li>
+                <li>
+                  <a href='#'>Templates</a>
+                </li>
+                <li>
+                  <a href='#'>Integrations</a>
+                </li>
+                <li>
+                  <a href='#'>Customers</a>
+                </li>
+                <li>
+                  <a href='#'>Enterprise</a>
+                </li>
+                <li>
+                  <a href='#'>Pricing</a>
+                </li>
+              </ul>
+            </div>
+
+            <div className={styles['navbar-right']}>
+              <ul>
+                <li>
+                  <a href='#'>Contact</a>
+                </li>
+                <li>
+                  <a href='#'>Login</a>
+                </li>
+                <li>
+                  <a className={styles['sign-up']} href='#'>Sign Up</a>
+                </li>
+              </ul>
+            </div>
+          </header>
+        </div>
+
+        {/* Body */}
+
+
+        {/* Footer */}
+
+      </div>
+    </>
+  )
 }
